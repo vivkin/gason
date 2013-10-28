772b10d74ab5f56f4417232c14191c952730d1af
Ivan Vashchaev <vivkin@gmail.com>
2013-10-28 16:29:39 +0400

    download and run test suite

--- get-test-suite.sh
+++ get-test-suite.sh
@@ -1,2 +1,29 @@
 #!/bin/sh
-curl http://www.json.org/JSON_checker/test.zip | tar -x
+
+if [ -d "test" ]; then
+	echo "Test-suite already downloaded"
+else
+	curl http://www.json.org/JSON_checker/test.zip | tar -x
+fi
+
+fuckup() {
+	PASS=0
+	FAIL=0
+	for f in $1; do
+		debug/gason-pp "$f" &> /dev/null
+		if [ $? == 0 ]; then
+			let PASS++
+			echo "> $f GOOD"
+		else
+			let FAIL++
+			echo "> $f BAD"
+		fi
+	done
+	echo "=== $PASS/$FAIL"
+}
+
+echo "Processing fails...";
+fuckup "test/fail*.json"
+
+echo "Processing pass...";
+fuckup "test/pass*.json"
