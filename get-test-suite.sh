62f7e480c00335c9de0292769c68a642eb1a807b
foo <vivkin@gmail.com>
2013-10-30 05:07:47 +0400

    fix test script in ubuntu where bash is not a bash

--- get-test-suite.sh
+++ get-test-suite.sh
@@ -3,25 +3,25 @@
 if [ -d "test" ]; then
 	echo "Test-suite already downloaded"
 else
-	curl http://www.json.org/JSON_checker/test.zip | tar -x
+	curl -sSO http://www.json.org/JSON_checker/test.zip && unzip test.zip 2> /dev/null && rm test.zip
 fi
 
-fuckup() {
+count_pass_fail() {
 	PASS=0
 	FAIL=0
 	for f in $1; do
 		debug/gason-pp "$f" > /dev/null
-		if [ $? == 0 ]; then
-			let PASS++
+		if [ "$?" = 0 ]; then
+			PASS=$((PASS=PASS+1))
 		else
-			let FAIL++
+			FAIL=$((FAIL=FAIL+1))
 		fi
 	done
-	echo "=== $PASS/$[PASS+FAIL]"
+	echo "=== $PASS/$((PASS+FAIL))"
 }
 
 echo "Processing fails...";
-fuckup "test/fail*.json"
+count_pass_fail "test/fail*.json"
 
 echo "Processing pass...";
-fuckup "test/pass*.json"
+count_pass_fail "test/pass*.json"
