0fe813eb04f1ff55d81619b5679fc21e26d115f9
Ivan Vashchaev <vivkin@gmail.com>
2014-11-08 21:55:58 +0300

    Remove old benchmark and add refresh data for benchmarks

--- download.sh
+++ download.sh
@@ -1,20 +1,6 @@
-if [ ! -d shootout ]; then
-    set -x
-
-    mkdir shootout
-    cd shootout
-
-    [ ! -d sajson ] && git clone https://github.com/chadaustin/sajson.git
-    [ ! -d rapidjson ] && git clone https://github.com/miloyip/rapidjson
-
-    curl -fLO https://raw.githubusercontent.com/Newbilius/big_json_import_demo/master/test_data/big.json
-    curl -fLO https://raw.githubusercontent.com/RichardHightower/json-parsers-benchmark/master/data/citm_catalog.json
-    curl -fLo monster.json https://raw.githubusercontent.com/mrdoob/three.js/master/examples/models/animated/monster/monster.js
-fi
-
-#(mkdir -p build-android && cd build-android && cmake -DANDROID_NDK=/Users/viv/android/ndk \
-#    -DANDROID_ABI=armeabi-v7a \
-#    -DANDROID_NATIVE_API_LEVEL=14 \
-#    -LIBRARY_OUTPUT_PATH=../ruberoid/libs/armeabi-v7a \
-#    -DCMAKE_TOOLCHAIN_FILE=../android.toolchain.cmake \
-#    ..)
+set -x
+[ ! -d rapidjson ] && git clone https://github.com/miloyip/rapidjson
+[ ! -f canada.json ] && curl -fLO https://raw.githubusercontent.com/mloskot/json_benchmark/master/data/canada.json
+[ ! -f big.json ] && curl -fLO https://raw.githubusercontent.com/Newbilius/big_json_import_demo/master/test_data/big.json
+[ ! -f citm_catalog.json ] && curl -fLO https://raw.githubusercontent.com/RichardHightower/json-parsers-benchmark/master/data/citm_catalog.json
+[ ! -f marine.json ] && curl -fLo marine.json https://raw.githubusercontent.com/mrdoob/three.js/master/examples/models/skinned/marine/marine.js
